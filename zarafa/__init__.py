34e3e54dce3141a43425e270eec50e069a228c33
Jelle van der Waa <jelle@vdwaa.nl>
2014-10-04 22:10:54 +0200

    Add try/except to last_logon/last_logof since a new user has no logon property

--- zarafa/__init__.py
+++ zarafa/__init__.py
@@ -859,12 +859,18 @@ class Store(object):
     @property
     def last_logon(self):
         """ Return :datetime Last logon of a user on this store """
-        return self.prop(PR_LAST_LOGON_TIME).value or None
+        try:
+            return self.prop(PR_LAST_LOGON_TIME).value
+        except MAPIErrorNotFound:
+            return None
 
     @property
     def last_logoff(self):
         """ Return :datetime of the last logoff of a user on this store """
-        return self.prop(PR_LAST_LOGOFF_TIME).value or None
+        try:
+            return self.prop(PR_LAST_LOGOFF_TIME).value
+        except:
+            return None
 
     def prop(self, proptag):
         return _prop(self, self.mapiobj, proptag)
