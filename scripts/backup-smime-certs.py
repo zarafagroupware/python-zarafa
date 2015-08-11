f041bb93f1c9784990bc74ae03764b47860da525
markb <m.debruijn@zarafa.com>
2015-08-11 16:29:53 +0200

    Update to work with the latest version of the Zarafa WebApp S/mime plugin.

--- scripts/backup-smime-certs.py
+++ scripts/backup-smime-certs.py
@@ -5,16 +5,21 @@ import base64
 
 z = zarafa.Server()
 
+
 def main():
     for user in z.users(remote=True):
         for item in user.store.root.associated.items():
-            if item.prop(PR_MESSAGE_CLASS_A).value == 'Webapp.Security.Private':
-                pkcs12 = item.body.text
-                fname = "%s.p12" % user.email
-                print "Dumping %s" % fname
-                f = open(fname, 'w')
-                f.write(base64.standard_b64decode(pkcs12))
-                f.close()
+
+            fname = user.name
+            if item.prop(PR_MESSAGE_CLASS_W):
+                if item.prop(PR_MESSAGE_CLASS_W).value.lower() == 'webapp.security.private':
+                    with open(fname + '.p12', 'w') as f:
+                        f.write(base64.b64decode(item.body.text))
+
+                if item.prop(PR_MESSAGE_CLASS_W).value.lower() == 'webapp.security.public':
+                    with open(fname + '.pub.pem', 'w') as f:
+                        f.write(base64.b64decode(item.body.text))
+
 
 if __name__ == '__main__':
     main()
