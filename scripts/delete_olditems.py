46f34ec16a6dcb8892eed0fab82c7a63a0f8a758
Jelle van der Waa <jelle@vdwaa.nl>
2015-11-06 23:00:01 +0100

    Refactor delete_olditems
    Remove the assert and use something more userfriendly.
    Users don't want to read a stacktrace..
    
    Print the timedelta of the two datetime's instead of
    calculating the difference yourself. Also clean up the
    datetime imports.
    
    Lastly fix a spelling error in the error string.

--- scripts/delete_olditems.py
+++ scripts/delete_olditems.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 import zarafa
-from MAPI.Util import *
-from types import *
-import datetime
+from datetime import datetime, timedelta
 import time
 import sys
 
@@ -13,29 +11,17 @@ def opt_args():
     parser.add_option('-v', '--verbose', dest='verbose', action='store_true', help='enable verbose mode')
     return parser.parse_args()
 
-
-def getMailAge(timestamp):
-    messagetime = datetime.datetime.fromtimestamp(timestamp)
-    curtime = datetime.datetime.now()
-    tdiff = curtime - messagetime
-    days = tdiff.days
-    minutes, seconds = divmod(tdiff.seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    remdays = '{0} days '.format(days) if days > 0 else ''
-    remhours = '{0} hours '.format(hours) if hours > 0 else ''
-    mailage = '%s%s%s minutes' % (remdays, remhours, minutes)
-    return mailage
-
-
 def main():
     options, args = opt_args()
 
-    assert args, 'You must specify the amount of days!'
+    if not args:
+        print 'You must specify the amount of days!'
+        sys.exit(1)
 
     try:
         val = int(args[0])
     except ValueError:
-        return sys.exit("Days not specify as a number!")
+        return sys.exit("Days not specified as a number!")
 
     for user in zarafa.Server(options=options).users(parse=True):
         print 'Running for user:', user.name
@@ -44,10 +30,9 @@ def main():
             for item in folder.items():
                 if not item.received:
                     continue
-                if item.received.date() < datetime.date.today()-datetime.timedelta(days=int(args[0])):
+                if item.received < datetime.today() - timedelta(days=int(args[0])):
                     if options.verbose:
-                        print 'Item:', item.subject, 'Received:',
-                        getMailAge(item.prop(PR_MESSAGE_DELIVERY_TIME).mapi_value.unixtime)
+                        print 'Item:', item.subject, 'Received:', datetime.today() - item.received
                     else:
                         print 'Item:', item.subject
                     if options.modify:
