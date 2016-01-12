977e66e0d1508695d9ce06c111634aee5efdae59
Mark de Bruijn <bkram@users.noreply.github.com>
2016-01-12 12:08:44 +0100

    Add switch to disable ham functionality,  general code cleanup more pep compliant, add shell script to launch python script needed when run from cron etc.

--- scripts/zarafa-spamhandler/zarafa-spamhandler.py
+++ scripts/zarafa-spamhandler/zarafa-spamhandler.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import ConfigParser
-import sys
-import re
-import subprocess
 import datetime
-import zarafa
+import re
 import shlex
+import subprocess
+
+import zarafa
+
 delcounter = 0
 
 
@@ -13,7 +14,8 @@ def main():
     global delcounter
     learncounter = 0
     hamlearncounter = 0
-    (users, allusers, remoteusers, autolearn, autodelete, deleteafter, spamcommand, hamfolder, hammarkertoremove, hamfoldercreate, hamcommand, hamlimit) = getconfig()
+    (users, allusers, remoteusers, autolearn, autodelete, deleteafter, spamcommand, hamfolder, hammarkertoremove,
+     hamfoldercreate, hamcommand, hamlimit, autoham) = getconfig()
     z = zarafa.Server()
     if allusers and not users:
         users = []
@@ -23,43 +25,47 @@ def main():
         try:
             user = z.user(username)
             inboxelements = 0
-            try:
-               nospamFolder = user.store.folder(hamfolder)
-            except:
-               if(hamfoldercreate):
-                  print "%s : create ham folder [%s]" % (user.name, hamfolder)
-                  nospamFolder = user.store.subtree.create_folder(hamfolder)
-               else:
-                  print "%s : has not ham folder [%s]" % (user.name, hamfolder)
-                  continue
-            p = re.compile(hammarkertoremove)
-            for item in nospamFolder.items():
-                if (hamlimit > 0 and inboxelements > hamlimit):
-                    break
-                if autolearn:
-                    if (item.header('x-spam-flag') and item.header('x-spam-flag') == 'YES'):
-                        inboxelements += 1
-                        print "%s : tagged spam in inbox [Subject: %s]" % (user.name, item.subject)
-                        try:
-                            hp = subprocess.Popen(shlex.split(hamcommand), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-                            learn, ham_output_error = hp.communicate(item.eml())
-                        except:
-                            print "failed to run [HAM] [%s]" % ham_output_err
-                        if learn:
-                            item.subject=p.sub('',item.subject)
-                            nospamFolder.move(item, user.store.inbox)
-                            print "%s : learned [%s]" % (user.name, learn.rstrip('\n'))
-                            hamlearncounter += 1
-                        continue
+            print autoham
+            if autoham:
+                try:
+                    nospamfolder = user.store.folder(hamfolder)
+                except:
+                    if hamfoldercreate:
+                        print "%s : create ham folder [%s]" % (user.name, hamfolder)
+                        nospamfolder = user.store.subtree.create_folder(hamfolder)
+                    else:
+                        print "%s : has no ham folder [%s]" % (user.name, hamfolder)
+
+                p = re.compile(hammarkertoremove)
+                for item in nospamfolder.items():
+                    if 0 < hamlimit < inboxelements:
+                        break
+                    if autolearn:
+                        if item.header('x-spam-flag') and item.header('x-spam-flag') == 'YES':
+                            inboxelements += 1
+                            print "%s : tagged spam in inbox [Subject: %s]" % (user.name, item.subject)
+                            try:
+                                hp = subprocess.Popen(shlex.split(hamcommand), stdin=subprocess.PIPE,
+                                                      stdout=subprocess.PIPE)
+                                learn, ham_output_error = hp.communicate(item.eml())
+                            except:
+                                print "failed to run [HAM] [%s]" % ham_output_err
+                            if learn:
+                                item.subject = p.sub('', item.subject)
+                                nospamfolder.move(item, user.store.inbox)
+                                print "%s : learned [%s]" % (user.name, learn.rstrip('\n'))
+                                hamlearncounter += 1
+
             for item in user.store.junk.items():
                 if autolearn:
                     if (not item.header('x-spam-flag')) or (item.header('x-spam-flag') == 'NO'):
                         print "%s : untagged spam [Subject: %s]" % (user.name, item.subject)
                         try:
-                            p = subprocess.Popen(shlex.split(spamcommand), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                            p = subprocess.Popen(shlex.split(spamcommand), stdin=subprocess.PIPE,
+                                                 stdout=subprocess.PIPE)
                             learn, output_err = p.communicate(item.eml())
                         except:
-                            print 'failed to run [%s] [%s]' % (SPAM, output_err)
+                            print 'failed to run [SPAM] [%s]' % output_err
                         if learn:
                             print "%s : learned [%s]" % (user.name, learn.rstrip('\n'))
                             delmsg = 'delete after learn'
@@ -67,7 +73,7 @@ def main():
                             learncounter += 1
                         continue
                 if autodelete:
-                    if item.received.date() < (datetime.date.today()-datetime.timedelta(days=deleteafter)):
+                    if item.received.date() < (datetime.date.today() - datetime.timedelta(days=deleteafter)):
                         delmsg = 'autodelete'
                         deletejunk(user, item, delmsg)
         except Exception as error:
@@ -89,27 +95,30 @@ def deletejunk(user, item, delmsg):
 
 
 def getconfig():
-    Config = ConfigParser.ConfigParser()
+    config = ConfigParser.ConfigParser()
     try:
-        Config.read('zarafa-spamhandler.cfg')
-        users = Config.get('users', 'users')
-        remoteusers = Config.getboolean('users', 'remoteusers')
-        autolearn = Config.getboolean('learning', 'autolearn')
-        autodelete = Config.getboolean('deleting', 'autodelete')
-        deleteafter = Config.getint('deleting', 'deleteafter')
-        spamcommand = Config.get('spamcommand', 'command')
-        hamfolder = Config.get('ham', 'folder')
-        hamfoldercreate = Config.getboolean('ham', 'create')
-        hammarkertoremove = Config.get('ham', 'marker')
-        hamcommand = Config.get('ham', 'command')
-        hamlimit = Config.get('ham', 'limit')
+        config.read('zarafa-spamhandler.cfg')
+        users = config.get('users', 'users')
+        remoteusers = config.getboolean('users', 'remoteusers')
+        autolearn = config.getboolean('learning', 'autolearn')
+        autodelete = config.getboolean('deleting', 'autodelete')
+        deleteafter = config.getint('deleting', 'deleteafter')
+        spamcommand = config.get('spamcommand', 'command')
+        hamfolder = config.get('ham', 'folder')
+        hamfoldercreate = config.getboolean('ham', 'create')
+        hammarkertoremove = config.get('ham', 'marker')
+        hamcommand = config.get('ham', 'command')
+        hamlimit = config.get('ham', 'limit')
+        autoham = config.getboolean('ham', 'autoham')
 
         if not users:
             allusers = True
         else:
             allusers = False
             users = users.replace(" ", "").split(",")
-        return (users, allusers, remoteusers, autolearn, autodelete, deleteafter, spamcommand, hamfolder, hammarkertoremove, hamfoldercreate, hamcommand, hamlimit)
+        return (
+            users, allusers, remoteusers, autolearn, autodelete, deleteafter, spamcommand, hamfolder, hammarkertoremove,
+            hamfoldercreate, hamcommand, hamlimit, autoham)
     except Exception as error:
         print error
         exit('Configuration error, please check zarafa-spamhandler.cfg')
---
+++ scripts/zarafa-spamhandler/zarafa-spamhandler.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export PYTHONIOENCODING=utf-8
+export LANG=en_US.UTF-8
+python zarafa-spamhandler.py
