bedd1673c3fbcba7d0e51d75cfa5d1d61534837b
markb <m.debruijn@zarafa.com>
2016-01-21 16:56:05 +0100

    Show start and or end if available

--- scripts/list-out-of-office.py
+++ scripts/list-out-of-office.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
 import zarafa
 
 server = zarafa.Server()
 for user in server.users():
     oof = user.outofoffice
     if oof.enabled:
-        # FIXME: show subject/timespan?
-        print 'User: %s' % (user.name)
+        print 'User: %s' % user.name
+        print oof.subject
+        if oof.start:
+            print 'Start: %s' % oof.start
+        if oof.end:
+            print 'End: %s' % oof.end
+
+server = zarafa.Server()
